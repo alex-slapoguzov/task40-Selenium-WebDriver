1f03b4b5c69115cc475e336a87d7b8a86ad31c62
Alexey Slapoguzov <alexeyslapoguzov@coherentsolutions.com>
2018-06-20 10:35:14 +0300

    add FramesTest

---
+++ src/test/java/FramesTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FramesTest {
+
+    private WebDriver driver;
+    private String URL = "https://the-internet.herokuapp.com/iframe";
+    private final String TEXT = "Hello world!";
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+        driver.get(URL);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void fremesTest() {
+        WebElement frameLocator = driver.findElement(By.id("mce_0_ifr"));
+        driver.switchTo().frame(frameLocator);
+        WebElement inputField = driver.findElement(By.id("tinymce"));
+        inputField.clear();
+        inputField.sendKeys(TEXT);
+        WebElement p = driver.findElement(By.xpath("//body[@id=\"tinymce\"]/p"));
+        Assert.assertEquals(p.getText(), TEXT, "Text " + "[" + TEXT + "]" + " is different!");
+
+    }
+}
